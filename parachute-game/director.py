bf6acdb256d647dfeab7d82f2dc861093dd92962
hyruleismyrule <hyruleismyrule@gmail.com>
2022-10-19 18:42:10 -0400

    cynthia - started director

---
+++ parachute-game/director.py
@@ -0,0 +1,16 @@
+# Imports
+# from # import #
+# Import the word
+
+class Director:
+    """A person who directs the game. 
+    
+    The responsibility of a Director is to control the sequence of play.
+
+    Attributes:
+        word (string): The word the player is trying to guess.
+        is_playing (boolean): Whether or not the game is being played.
+        guessed_letters (list): A list of guessed characters.
+        # ? Display Parachute
+        # ? Display word with underscores
+    """
